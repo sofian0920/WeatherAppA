62bac7a82e1166fa7a088d1f037490c18f3a0f56
Sofia <norinasofia@mail.ru>
2023-10-01 00:30:58 +0400

    create WeatherViewController

---
+++ WeatherAppAstone/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  WeatherAppAstone
+//
+//  Created by Софья Норина on 1.10.2023.
+//
+
+import Foundation
+
+
+struct WeatherData {
+    let tempruter: Double
+    var nameCity: String
+    let description: Int
+    let humidity: Double
+}
